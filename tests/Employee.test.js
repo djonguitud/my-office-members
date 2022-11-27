8b34cd1dcad5ad3a00acf6f77a25e3bab3d79cb1
Diego Jonguitud DellPc <djonguitud95@gmail.com>
2022-11-26 20:58:06 -0600

    merge: from test-b to main branch

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,1 +1,12 @@
 /* Finalmente, aunque no es un requisito, debe considerar agregar la validación para asegurar que la entrada de usuario proporcionada esté en el formato esperado adecuado. */
+'use strict';
+
+import Employee from '../lib/Employess.js';
+
+test('Deberá crearse un objeto de empleado con nombre, id y correo electrónico', () => {
+	const employeeOne = new Employee('Elon Musk', 12, 'emusk@gmail.com');
+
+	expect(employeeOne.name).toBe('Elon Musk');
+	expect(employeeOne.id).toBe(12);
+	expect(employeeOne.email).toBe('emusk@gmail.com');
+});
